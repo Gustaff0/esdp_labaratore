80b775775444c7596ecea88a4b4660c162839cd6
Gustaff0 <barsbekclash@gmail.com>
2021-08-19 22:01:56 +0600

    Создание моделей, накатка миграции

--- announcement/accounts/models.py
+++ announcement/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+
+class Number(models.Model):
+    number = models.IntegerField(blank=False, null=False, verbose_name='Номер', validators=[MinLengthValidator(8)])
+    user = models.OneToOneField(get_user_model(), blank=False, null=False, related_name='number', verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Numbers'
+        verbose_name = 'Номер'
+        verbose_name_plural = 'Номера'
--- announcement/api_v1/models.py
+++ announcement/api_v1/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 
 # Create your models here.
+
+class Announcement(models.Model):
+    description = models.CharField(max_length=200, null=False, blank=False, validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(get_user_model(), blank=False, null=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    number = models.IntegerField(null=True, blank=True, validators=[MinLengthValidator(8)])
+
+    class Meta:
+        db_table = 'Announcements'
+        verbose_name = 'Обьявление'
+        verbose_name_plural = 'Обьявлении'
+
+
+class Photo(models.Model):
+    photo = models.ImageField(blank=False, null=False, upload_to='announcement_pics')
+    announcement = models.ForeignKey('api_v1.Announcement', blank=False, null=False, related_name='photo', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Photos'
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
