480d4fd26719b8e0fac472ab9e89b5eabd9f6e74
Gustaff0 <barsbekclash@gmail.com>
2021-08-19 22:32:48 +0600

    Регистрация моделей в Админке

--- announcement/accounts/admin.py
+++ announcement/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from accounts.models import Number
 
 # Register your models here.
+
+class NumberAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'user']
+    list_filter = ['user']
+    search_fields = ['user']
+    fields = ['id', 'number', 'user']
+    readonly_fields = ['id']
+
+
+admin.site.register(Number, NumberAdmin)
--- announcement/api_v1/admin.py
+++ announcement/api_v1/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from api_v1.models import Announcement, Photo
 
 # Register your models here.
+
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'author', 'created_at']
+    list_filter = ['author', 'created_at']
+    search_fields = ['author', 'description']
+    fields = ['id', 'description', 'author', 'created_at']
+    readonly_fields = ['id', 'created_at']
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'photo', 'announcement']
+    list_filter = ['announcement']
+    search_fields = ['announcement']
+    fields = ['id', 'photo', 'announcement']
+
+
+admin.site.register(Announcement, AnnouncementAdmin)
+admin.site.register(Photo, PhotoAdmin)
